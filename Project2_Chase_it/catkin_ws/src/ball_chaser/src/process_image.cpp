15cb714d4fdf04ada865fec7a47a8f0270ce35a1
Sergey Mishakin <mishakin@arrival.com>
2023-11-04 12:41:00 +0100

    process_image implemented

--- Project2_Chase_it/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ Project2_Chase_it/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -8,7 +8,14 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
+    ROS_INFO_STREAM("Driving the robot");
+    ball_chaser::DriveToTarget srv;
+    srv.request.linear_x = lin_x;
+    srv.request.angular_z = ang_z;
+
+    if (!client.call(srv))
+        ROS_ERROR("Failed to drive the robot");
 }
 
 // This callback function continuously executes and reads the image data
@@ -16,11 +23,52 @@ void process_image_callback(const sensor_msgs::Image img)
 {
 
     int white_pixel = 255;
+    float angular_velocity_val = 0.5;
+    float linear_velocity_val = 0.5;
+    float left_border_normed = 0.33F;
+    float right_border_normed = 0.66F;
 
-    // TODO: Loop through each pixel in the image and check if there's a bright white one
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
+    bool has_white_pixel = false;
+    float white_pixel_col_normed = -1.0;
+
+    float ball_projection_row = 0;
+    for (int row = 0; row < img.height; ++row)
+    {
+        for (int byte_idx = 0; byte_idx < img.step; ++byte_idx)
+        {
+            float col_normed = static_cast<float>(byte_idx) / static_cast<float>(img.step);
+            auto brightness = static_cast<int>(img.data[row * img.step + byte_idx]);
+            if (brightness == white_pixel)
+            {
+                has_white_pixel = true;
+                white_pixel_col_normed = col_normed;
+                break;
+            }
+        }
+        if (has_white_pixel)
+            break;
+    }
+
+    if (!has_white_pixel || white_pixel_col_normed <= 0.0F || white_pixel_col_normed >= 1.0F)
+    {
+        drive_robot(0.0F, 0.0F);
+        ROS_INFO_STREAM("No white ball was detected, robot stopped.");
+    }
+    else if (white_pixel_col_normed < left_border_normed)
+    {
+        drive_robot(0.0F, angular_velocity_val);
+        ROS_INFO_STREAM("White ball is to the left, rotating robot to the left");
+    }
+    else if (white_pixel_col_normed > right_border_normed)
+    {
+        drive_robot(0.0F, -angular_velocity_val);
+        ROS_INFO_STREAM("White ball is to the right, rotating robot to the right");
+    }
+    else
+    {
+        drive_robot(linear_velocity_val, 0.0F);
+        ROS_INFO_STREAM("White ball is ahead, moving robot forward");
+    }
 }
 
 int main(int argc, char **argv)
