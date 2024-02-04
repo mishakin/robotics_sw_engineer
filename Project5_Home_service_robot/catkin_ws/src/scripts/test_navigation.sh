2e430fd1a062d4871667894a22e229f1215f81d1
Sergey Mishakin <mishakinsergey@gmail.com>
2024-02-04 21:44:15 +0100

    Test navigation in progress

--- Project5_Home_service_robot/catkin_ws/src/scripts/test_navigation.sh
+++ Project5_Home_service_robot/catkin_ws/src/scripts/test_navigation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " roslaunch my_robot my_robot_world.launch " &
 sleep 3
-xterm  -e  " roslaunch my_robot acml_demo.launch " &
+xterm  -e  " roslaunch my_robot amcl_demo.launch " &
 sleep 3
 xterm  -e  " roslaunch my_robot view_navigation.launch " &
 
