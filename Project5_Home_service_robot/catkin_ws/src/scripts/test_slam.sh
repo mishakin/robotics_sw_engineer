b1d27efa2526f31289327191a57e245451d16e2d
Sergey Mishakin <mishakinsergey@gmail.com>
2024-02-04 12:35:55 +0100

    Clean up

---
+++ Project5_Home_service_robot/catkin_ws/src/scripts/test_slam.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+xterm  -e  " roslaunch my_robot my_robot_world.launch " &
+sleep 3
+xterm  -e  " roslaunch my_robot gmapping_demo.launch " &
+sleep 3
+xterm  -e  " roslaunch my_robot view_navigation.launch " &
+sleep 3
+xterm  -e  " rosrun teleop_twist_keyboard teleop_twist_keyboard.py " &
